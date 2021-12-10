c8cb6a541443fbce15811e678ca8a8ddd1c8101a
thanhdien0208 <diennguyen.blog@gmail.com>
2021-12-11 03:29:19 +0700

    upload

--- js/snapShot.js
+++ js/snapShot.js
@@ -1,82 +1,97 @@
 
-  (function () {
-  if (
-  !"mediaDevices" in navigator ||
-  !"getUserMedia" in navigator.mediaDevices
-  ) {
-  document.write('Not support API camera')
-  return;
-  }
+(function () {
+if (
+!"mediaDevices" in navigator ||
+!"getUserMedia" in navigator.mediaDevices
+) {
+document.write('Not support API camera')
+return;
+}
 
-  const video = document.querySelector("#video");
-  const canvas = document.querySelector("#canvas");
-  const screenshotsContainer = document.querySelector("#screenshotsContainer");
-  let videoStream = null
-  let useFrontCamera = true; //camera trước
-  const constraints = {
-  video: {
-      width: {
-      min: 1280,
-      ideal: 1920,
-      max: 2560,
-      },
-      height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440,
-      }
-  },
-  };
+const video = document.querySelector("#video");
+const canvas = document.querySelector("#canvas");
+const screenshotsContainer = document.querySelector("#screenshotsContainer");
+let videoStream = null
+let useFrontCamera = true; //camera trước
+const constraints = {
+video: {
+    width: {
+    min: 1280,
+    ideal: 1920,
+    max: 2560,
+    },
+    height: {
+    min: 720,
+    ideal: 1080,
+    max: 1440,
+    }
+},
+};
 
-  // play
-  btnPlay.addEventListener("click", function () {
-  video.play();
-  btnPlay.classList.add("is-hidden");
-  btnPause.classList.remove("is-hidden");
-  });
+// play
+btnPlay.addEventListener("click", function () {
+video.play();
+btnPlay.classList.add("is-hidden");
+btnPause.classList.remove("is-hidden");
+});
 
-  // pause
-  btnPause.addEventListener("click", function () {
-  video.pause();
-  btnPause.classList.add("is-hidden");
-  btnPlay.classList.remove("is-hidden");
-  });
-
-
-  btnChangeCamera.addEventListener("click", function () {
-  useFrontCamera = !useFrontCamera;
-  init();
-  });
+// pause
+btnPause.addEventListener("click", function () {
+video.pause();
+btnPause.classList.add("is-hidden");
+btnPlay.classList.remove("is-hidden");
+});
 
 
-  function stopVideoStream() {
-  if (videoStream) {
-      videoStream.getTracks().forEach((track) => {
-      track.stop();
-      });
-  }
-  }
+btnChangeCamera.addEventListener("click", function () {
+useFrontCamera = !useFrontCamera;
+init();
+});
 
-  btnScreenshot.addEventListener("click", function () {
-    const img = document.createElement("img");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext("2d").drawImage(video, 0, 0);
-    img.src = canvas.toDataURL("image/png");
-    screenshotsContainer.prepend(img);
-  });
 
-  async function init() {
-  stopVideoStream();
-  constraints.video.facingMode = useFrontCamera ? "user" : "environment";
-  try {
-      videoStream = await navigator.mediaDevices.getUserMedia(constraints);
-      video.srcObject = videoStream;
-  } catch (error) {
-      console.log(error)
-  }
-  }
-  init();
-  })();
+function stopVideoStream() {
+if (videoStream) {
+    videoStream.getTracks().forEach((track) => {
+    track.stop();
+    });
+}
+}
+// Chụp nhiều ảnh
+//   btnScreenshot.addEventListener("click", function () {
+//     const img = document.createElement("img");
+//     canvas.width = video.videoWidth;
+//     canvas.height = video.videoHeight;
+//     canvas.getContext("2d").drawImage(video, 0, 0);
+//     img.src = canvas.toDataURL("image/png");
+//     screenshotsContainer.prepend(img);
+//   });
+
+// Chụp 1 ảnh
+btnScreenshot.addEventListener("click", function () {
+let img = document.getElementById('screenshot');
+if (!img) {
+    img = document.createElement("img");
+    img.id = 'screenshot';
+    img.style.width = '30%';
+}
+canvas.width = video.videoWidth;
+canvas.height = video.videoHeight;
+canvas.getContext("2d").drawImage(video, 0, 0);
+img.src = canvas.toDataURL("image/png");
+screenshotsContainer.prepend(img);
+});
+
+async function init() {
+stopVideoStream();
+constraints.video.facingMode = useFrontCamera ? "user" : "environment";
+try {
+    videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+    video.srcObject = videoStream;
+} catch (error) {
+    console.log(error)
+}
+}
+init();
+})();
 
 
